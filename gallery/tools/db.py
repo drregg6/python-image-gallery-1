9bb432ed9f1ca0e1dc15839436dcbe0007e9dbcc
C. David Shaffer <cdshaffer@acm.org>
2022-05-16 14:13:32 -0400

    Database library now exports a class DbConnection that encapsulates a connection.  This connection should not be shared across multiple threads!

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -2,43 +2,61 @@ import psycopg2
 import json
 from secrets import get_secret_image_gallery
 
-connection = None
 
-def get_secret():
-    jsonString = get_secret_image_gallery()
-    return json.loads(jsonString)
+def read_secret_from_aws():
+    json_string = get_secret_image_gallery()
+    return json.loads(json_string)
+
 
 def get_password(secret):
     return secret['password']
 
+
 def get_host(secret):
     return secret['host']
+
 
 def get_username(secret):
     return secret['username']
 
+
 def get_dbname(secret):
     return secret['database_name']
 
-def connect():
-    global connection
-    secret = get_secret()
-    connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret), password=get_password(secret))
 
-def execute(query,args=None):
-    global connection
-    cursor = connection.cursor()
-    if not args:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, args)
-    return cursor
+class DbConnection:
+    secret = None
+
+    @classmethod
+    def get_secret(cls):
+        if cls.secret is not None:
+            return cls.secret
+        secret = read_secret_from_aws()
+
+    def __init__(self):
+        self.connection = None
+
+    def connect(self):
+        secret = self.get_secret()
+        self.connection = psycopg2.connect(host=get_host(secret), dbname=get_dbname(secret), user=get_username(secret),
+                                           password=get_password(secret))
+
+    def execute(self, query, args=None):
+        cursor = self.connection.cursor()
+        if not args:
+            cursor.execute(query)
+        else:
+            cursor.execute(query, args)
+        return cursor
+
 
 def main():
-    connect()
-    res = execute('select * from users')
+    db = DbConnection()
+    db.connect()
+    res = db.execute('select * from users')
     for row in res:
         print(row)
+
 
 if __name__ == '__main__':
     main()
